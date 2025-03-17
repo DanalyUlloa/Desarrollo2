72908172c53e36e388dc48e29c98892b5365f055
DanalyUlloa <danaly.ulloa@uabc.edu.mx>
2025-03-16 22:22:44 -0700

    Profesores
    Agregación de Profesores

---
+++ entidad/src/main/java/mx/desarrollo/entidad/Profesores.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.desarrollo.entidad;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author danal
+ */
+@Entity
+@Table(name = "profesores")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Profesores.findAll", query = "SELECT p FROM Profesores p")
+    , @NamedQuery(name = "Profesores.findByIdProfesor", query = "SELECT p FROM Profesores p WHERE p.idProfesor = :idProfesor")
+    , @NamedQuery(name = "Profesores.findByNombre", query = "SELECT p FROM Profesores p WHERE p.nombre = :nombre")
+    , @NamedQuery(name = "Profesores.findByApellidoPaterno", query = "SELECT p FROM Profesores p WHERE p.apellidoPaterno = :apellidoPaterno")
+    , @NamedQuery(name = "Profesores.findByApellidoMaterno", query = "SELECT p FROM Profesores p WHERE p.apellidoMaterno = :apellidoMaterno")
+    , @NamedQuery(name = "Profesores.findByRfc", query = "SELECT p FROM Profesores p WHERE p.rfc = :rfc")})
+public class Profesores implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idProfesor")
+    private Integer idProfesor;
+    @Basic(optional = false)
+    @Column(name = "Nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "ApellidoPaterno")
+    private String apellidoPaterno;
+    @Basic(optional = false)
+    @Column(name = "ApellidoMaterno")
+    private String apellidoMaterno;
+    @Basic(optional = false)
+    @Column(name = "RFC")
+    private String rfc;
+
+    public Profesores() {
+    }
+
+    public Profesores(Integer idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public Profesores(Integer idProfesor, String nombre, String apellidoPaterno, String apellidoMaterno, String rfc) {
+        this.idProfesor = idProfesor;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.rfc = rfc;
+    }
+
+    public Integer getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(Integer idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProfesor != null ? idProfesor.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Profesores)) {
+            return false;
+        }
+        Profesores other = (Profesores) object;
+        if ((this.idProfesor == null && other.idProfesor != null) || (this.idProfesor != null && !this.idProfesor.equals(other.idProfesor))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.desarrollo.entidad.Profesores[ idProfesor=" + idProfesor + " ]";
+    }
+    
+}
